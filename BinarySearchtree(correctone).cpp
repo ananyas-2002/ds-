0f135673c59c3c0ed334f5ba6a9525044cc38e78
ananyas-2002 <149650933+ananyas-2002@users.noreply.github.com>
2023-12-14 11:11:30 +0530

    Add files via upload

---
+++ BinarySearchtree(correctone).cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+	int data;
+	node*Llink;
+	node*Rlink;
+};
+class binarysearchtree
+{
+	public:
+		node*createnode(int val)
+		{
+			node*tmp=new node;
+			tmp->data=val;
+			tmp->Llink=NULL;
+			tmp->Rlink=NULL;
+			return tmp;
+		}
+		node* insert(node*node,int data)
+		{		
+			if(node==NULL)
+			{
+				return createnode(data);
+			}
+			if(data <= node->data)
+		{
+			node->Llink = insert(node->Llink, data);
+		}
+		else if(data > node->data)
+		{
+			node->Rlink = insert(node->Rlink, data);
+		}
+		return node;
+	}
+     node*deletion(node*root,int value)
+    {
+    	if(root==NULL)
+    	{
+    		return root;
+		}
+		if(root->data>value)
+		{
+			root->Llink=deletion(root->Llink,value);
+			return root;
+		}
+		else if(root->data<value)
+		{
+			root->Rlink=deletion(root->Rlink,value);
+			return root;
+		}
+		if (root->Llink == NULL)
+		 {
+            node*temp = root->Rlink;
+            delete root;
+            return temp;
+        }
+    else if (root->Rlink == NULL) 
+	{
+        node*temp = root->Llink;
+        delete root;
+        return temp;
+    }
+ 
+    else {
+ 
+        node*succParent = root;
+        node*succ = root->Rlink;
+        while (succ->Llink != NULL)
+		 {
+            succParent = succ;
+            succ = succ->Llink;
+        }
+        if (succParent !=root)
+        {
+            succParent->Llink= succ->Rlink;
+        }
+        else
+        {
+           succParent->Rlink = succ->Rlink;
+		   root->data = succ->data;
+           delete succ;
+           return root;
+       }
+    }
+		
+	}
+		void inorder(node *node)
+		{
+			if(node!=NULL)
+			{
+				inorder(node->Llink);
+				cout<<node->data<<" ";
+				inorder(node->Rlink);
+			}
+		}
+			void preorder(node *node)
+		{
+			if(node!=NULL)
+			{
+				cout<<node->data<<" ";
+				preorder(node->Llink);
+				preorder(node->Rlink);
+			}
+		}
+		void postorder(node *node)
+		{
+			if(node!=NULL)
+			{
+				postorder(node->Llink);
+				postorder(node->Rlink);
+				cout<<node->data<<" ";
+			}
+		}
+};
+int main()
+{
+	
+	int n,key;
+	node * root = NULL;
+	binarysearchtree bst;
+	cout << "Enter number of elements : ";
+	cin >> n;
+	cout<<"Enter elements:";
+	for(int i = 0; i < n; i++)
+	{
+		int value;
+		cin >> value;
+		root = bst.insert(root, value);	
+	}
+	
+	cout<< "Inorder traversal : ";
+	bst.inorder(root);
+	cout<<endl;
+	cout<<"Preorder traversal:";
+	bst.preorder(root);
+	cout<<endl;
+	cout<<"Postorder traversal:";
+	bst.postorder(root);
+	cout<<endl;
+	cout<<"enter key to delete:";
+	cin>>key;
+	root=bst.deletion(root,key);
+	cout<<"Modified BST after deleteting a node:";
+	bst.inorder(root);
+	return 0;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+}
